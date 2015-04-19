c81c9c03a6139c4ba5ecf3db1985c18cac30b990
Flaise <Flaise.Saffron@gmail.com>
2015-04-19 16:30:48 -0500

    Non-default orthographic projection

--- core/src/testGDX/Main.kt
+++ core/src/testGDX/Main.kt
@@ -2,15 +2,26 @@ package testGDX
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Camera
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.viewport.ExtendViewport
 
-public class Main : ApplicationAdapter() {
-    var batch : SpriteBatch? = null
-    var img : Texture? = null
+public class Main: ApplicationAdapter() {
+    var batch: SpriteBatch? = null
+    var img: Texture? = null
+    var camera = OrthographicCamera()
+    var viewport = ExtendViewport(8f, 8f, 10f, 10f, camera)
 
     override fun create() {
+        camera.viewportWidth = 8f
+        camera.viewportHeight = 8f
+        camera.position.set(0f, 0f, 0f)
+        camera.update()
+        viewport.apply()
+
         batch = SpriteBatch()
         img = Texture("badlogic.jpg")
     }
@@ -19,10 +30,21 @@ public class Main : ApplicationAdapter() {
         val b = batch
         if(b == null)
             return
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+
+        val camera = viewport.getCamera()
+        b.setProjectionMatrix(camera.projection)
+        b.setTransformMatrix(camera.view)
+
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
         b.begin()
-        b.draw(img, 0f, 0f)
+        for(i in 0f..9f)
+            b.draw(img, i, i, 1f, 1f)
         b.end()
+    }
+
+    override fun resize(width: Int, height: Int) {
+        viewport.update(width, height)
     }
 }

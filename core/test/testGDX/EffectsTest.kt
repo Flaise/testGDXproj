8209601fc6123bbd66d348ba17e456ebc4f3fd84
Flaise <Flaise.Saffron@gmail.com>
2015-04-19 14:50:27 -0500

    More tests for effect handling

--- core/test/testGDX/EffectsTest.kt
+++ core/test/testGDX/EffectsTest.kt
@@ -6,19 +6,60 @@ import org.junit.After
 import org.junit.Before
 
 
-class MockEffect(val a: Int) {
+class MockEffect(var value: Int) {}
+
+class MockHandlerFirst(val expectedInput: Int):
+        EffectHandler<MockEffect>(javaClass<MockEffect>(), -1) {
+    override public fun invoke(context: Context, effect: MockEffect) {
+        assertEquals(effect.value, expectedInput)
+        effect.value += 1
+    }
+}
+
+class MockHandlerSecond(val expectedInput: Int):
+        EffectHandler<MockEffect>(javaClass<MockEffect>(), -2) {
+    override public fun invoke(context: Context, effect: MockEffect) {
+        assertEquals(effect.value, expectedInput)
+        effect.value += 2
+    }
 }
 
 
 public class EffectsTest {
+    var context = Context()
+    var effect = MockEffect(0)
 
     Before fun setUp() {
+        context = Context()
+        effect = MockEffect(0)
     }
 
     After fun tearDown() {
     }
 
-    Test fun applyEmpty() {
-        applyEffect(Context(), MockEffect(2))
+    Test fun applyZero() {
+        applyEffect(context, effect)
+        assertEquals(effect.value, 0)
+    }
+
+    Test fun applyOne() {
+        addEffectHandler(context, MockHandlerFirst(0))
+        applyEffect(context, effect)
+        assertEquals(effect.value, 1)
+    }
+
+    Test fun applyTwo() {
+        addEffectHandler(context, MockHandlerFirst(0))
+        addEffectHandler(context, MockHandlerSecond(1))
+        applyEffect(context, effect)
+        assertEquals(effect.value, 3)
+    }
+
+    Test fun removeHandler() {
+        val handler = MockHandlerFirst(-999999)
+        addEffectHandler(context, handler)
+        removeEffectHandler(context, handler)
+        applyEffect(context, effect)
+        assertEquals(effect.value, 0)
     }
 }
--- desktop/src/testGDX/desktop/DesktopLauncher.java
+++ desktop/src/testGDX/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import testGDX.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.backgroundFPS = 1;
 		new LwjglApplication(new Main(), config);
 	}
 }
